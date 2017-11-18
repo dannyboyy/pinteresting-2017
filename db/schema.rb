371c377a8c920e081b28f834829e1518a9c189b8
Danny Wu <wushuo1988@hotmail.com>
2017-11-18 12:23:15 -0800

    Generated pins scaffold and updated some views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118191658) do
+ActiveRecord::Schema.define(version: 20171118195233) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
