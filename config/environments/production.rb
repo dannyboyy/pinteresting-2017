dcb4f0760a7a15dcfb6e75e623b73cd9e825ee37
Danny Wu <wushuo1988@hotmail.com>
2017-11-18 11:31:25 -0800

    Implemented Devise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,11 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # Required for Heroku
+  # Note to set this to your actual host
+  config.action_mailer.default_url_options = { host: 'https://quiet-garden-88528.herokuapp.com/' }
+
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
